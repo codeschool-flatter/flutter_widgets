9193cd0b04b0eef13e55f18ed5fca27ba69b1293
ZARIFMAC <uzbzarif@gmail.com>
2023-04-01 15:07:15 +0500

    text style

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   Container x = Container();
+  Color red = Color(4294907025);
+  print(0xFFff1491);
+  print(0xFF);
   Text txt = Text(
     'Hello World',
     textDirection: TextDirection.ltr,
+    textAlign: TextAlign.center,
+    style: TextStyle(
+      fontSize: 50,
+      color: Colors.brown,
+      backgroundColor: Colors.white,
+      fontWeight: FontWeight.w100,
+    ),
   );
   runApp(txt);
 }
