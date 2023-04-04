c825a5e825ed109860b5e1f861e44de557226def
ZARIFMAC <uzbzarif@gmail.com>
2023-04-04 15:20:07 +0500

    alignment

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
 // Create blue Container
-  Container blue = Container(
-    color: Colors.blue,
-    margin: const EdgeInsets.all(50.0),
-  );
+
 // Create red Container with blue child
   Container red = Container(
     color: Colors.red,
-    child: blue,
-    margin: const EdgeInsets.all(10.0),
+    child: const Text(
+      'CODEACADEMY',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        fontSize: 30.0,
+        color: Colors.white,
+      ),
+    ),
+    alignment: Alignment.bottomLeft,
+    // margin: const EdgeInsets.all(50.0),
+    padding: const EdgeInsets.all(10.0),
   );
   runApp(red);
 }
